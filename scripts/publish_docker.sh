13d5dbcf6bffefad4bb80c41cbb685e0e1b022be
Dara Dermody <daradermody@gmail.com>
2024-02-26 12:00:13 +0000

    Improve package scripts

---
+++ scripts/publish_docker.sh
@@ -0,0 +1,11 @@
+set -e
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Must not have uncommitted changes"
+  exit 1
+fi
+
+SHA=$(git rev-parse HEAD)
+bun docker-build
+docker tag daradermody/siren-steps:latest daradermody/siren-steps:${SHA}
+docker push daradermody/siren-steps:latest daradermody/siren-steps:${SHA}
